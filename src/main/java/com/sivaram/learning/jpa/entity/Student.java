6fc75557360e50424f095b28548cf70ab81cb9fa
ramuprabakaran <subs.prabakaran@gmail.com>
2021-07-21 22:54:33 +0400

    Creating Student Entity | close #4

---
+++ src/main/java/com/sivaram/learning/jpa/entity/Student.java
@@ -0,0 +1,39 @@
+package com.sivaram.learning.jpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Student {
+
+	@Id
+	@SequenceGenerator(
+				name = "student_sequence",
+				sequenceName = "student_sequence",
+				allocationSize = 1
+			)
+	@GeneratedValue(
+				strategy = GenerationType.SEQUENCE,
+				generator = "student_sequence"
+			)
+	private String studentId;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String guardianName;
+	private String guardianEmail;
+	private String guardianMobile;
+	
+}
