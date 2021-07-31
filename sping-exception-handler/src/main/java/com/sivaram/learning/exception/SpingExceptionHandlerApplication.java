d7b05108cda9f9aff079dac2eb460b9092f6d321
ramuprabakaran <subs.prabakaran@gmail.com>
2021-07-31 11:12:10 +0400

    Initial Spring Boot Project Generation

---
+++ sping-exception-handler/src/main/java/com/sivaram/learning/exception/SpingExceptionHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.sivaram.learning.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingExceptionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingExceptionHandlerApplication.class, args);
+	}
+
+}
---
+++ sping-exception-handler/src/test/java/com/sivaram/learning/exception/SpingExceptionHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivaram.learning.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingExceptionHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
